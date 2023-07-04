3b76e0314f20f1717b323e308ec0c04625f22fc3
dobutamine <tim.antonius@outlook.com>
2023-07-04 22:42:03 +0200

    added container model

--- explain_core/core_models/Container.py
+++ explain_core/core_models/Container.py
@@ -2,14 +2,30 @@ from explain_core.base_models.Capacitance import Capacitance
 
 
 class Container(Capacitance):
+    # independent variables
+    vol_extra: float = 0.0
+
+    # local variables
+    _cps: list[Capacitance] = []
+
+    def init_model(self, model: object) -> bool:
+        # init the parent class
+        super().init_model(model)
+
+        # get a reference to all the model components this container contains
+        for comp in self.contained_components:
+            self._cps.append(self._model.models[comp])
+
+        return self._is_initialized
 
     # override the calc_model method as the blood capacitance has some specific actions
     def calc_model(self) -> None:
-        # calculate the volume of the container
-        self.calc_volume()
+        # get the current volume from all contained model components
+        for c in self._cps:
+            self.vol += c.vol
 
-        # do the cap actions
+        # add the extra volume
+        self.vol += self.vol_extra
+
+        # do the capacitans actions -> calculate pressure
         super().calc_model()
-
-    def calc_volume(self) -> None:
-        pass
