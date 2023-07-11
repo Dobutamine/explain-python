46593db2b4216d468aeae01c5cb7a510e7b77e2c
dobutamine <tim.antonius@outlook.com>
2023-07-12 00:02:37 +0200

    improved pda

--- explain_core/core_models/DuctusArteriosus.py
+++ explain_core/core_models/DuctusArteriosus.py
@@ -1,11 +1,14 @@
 from explain_core.base_models.BaseModel import BaseModel
 from explain_core.core_models.BloodResistor import BloodResistor
+from explain_core.helpers.TubeResistance import calc_resistance_tube
 
 
 class DuctusArteriosus(BaseModel):
     # independent variables
-    length: float = 0.01
-    diameter: float = 0.002
+    length: float = 10
+    diameter: float = 2
+    viscosity: float = 6.0
+    nonlin: float = 0.0
     no_flow: bool = False
 
     # dependent variables
@@ -19,8 +22,8 @@ class DuctusArteriosus(BaseModel):
         # initialize the parent class
         super().init_model(model)
 
-        # configure a ductus arteriosus component
-        _args = {
+        # instantiate a ductus arteriosus as a blood resistor
+        self._duct = BloodResistor(**{
             "name": "DA",
             "description": "ductus arteriosus",
             "model_type": "BloodResistor",
@@ -31,19 +34,22 @@ class DuctusArteriosus(BaseModel):
             "comp_to": "PA",
             "r_for": 40000.0,
             "r_back": 40000.0,
-            "r_k": 0
-        }
-        # instantiate a ductus arteriosus as a blood resistor
-        self._duct = BloodResistor(**_args)
+            "r_k": self.nonlin
+        })
 
         # initialize this blood resistor
         self._duct.init_model(model)
 
     def calc_model(self) -> None:
         # calculate the current ductus resistance and other properties
-        self.res = self.calc_resistance()
+        if self.diameter > 0:
+            self.res = calc_resistance_tube(
+                self.diameter, self.length, self.viscosity)
+        else:
+            self.res = 4000000
         self._duct.r_for = self.res
         self._duct.r_back = self.res
+        self._duct.r_k = self.nonlin
         self._duct.is_enabled = self.is_enabled
         self._duct.no_flow = self.no_flow
 
@@ -52,6 +58,3 @@ class DuctusArteriosus(BaseModel):
 
         # get the state of the blood resistor
         self.flow = self._duct.flow
-
-    def calc_resistance(self) -> float:
-        return 40000.0
--- explain_core/helpers/TubeResistance.py
+++ explain_core/helpers/TubeResistance.py
@@ -1,7 +1,7 @@
 import math
 
 
-def calc_resistance_tube(diameter, length, viscosity=6, flow=0,  nonlin=0):
+def calc_resistance_tube(diameter, length, viscosity=6):
     # resistance is calculated using Poiseuille's Law : R = (8 * n * L) / (PI * r^4)
 
     # we have to watch the units carefully where we have to make sure that the units in the formula are
@@ -21,7 +21,9 @@ def calc_resistance_tube(diameter, length, viscosity=6, flow=0,  nonlin=0):
 
     # calculate the resistance
     res: float = (8.0 * n_mmhgs * length_meters) / (math.pi *
-                                                    math.pow(radius_meters, 4)) + nonlin * math.pow(flow, 2)
+                                                    math.pow(radius_meters, 4))
 
     # convert resistance of mmHg * s / mm^3 to mmHg *s / l
     res = res / 1000.0
+
+    return res
