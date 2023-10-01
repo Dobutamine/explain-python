450a9b272a0c90e2a313eba9627146cf49bb196b
dobutamine <tajantonius@gmail.com>
2023-10-01 23:03:04 +0200

    added mob

--- explain_core/core_models/Mob.py
+++ explain_core/core_models/Mob.py
@@ -5,10 +5,22 @@ from explain_core.core_models.BloodTimeVaryingElastance import BloodTimeVaryingE
 from explain_core.core_models.BloodCapacitance import BloodCapacitance
 from explain_core.base_models.Resistor import Resistor
 from explain_core.functions.BloodComposition import set_blood_composition
+from explain_core.functions.ActivationFunction import activation_function
 
 class Mob(BaseModel):
     # independent variables
-    baseline_metabolism: float = 0.0            # vo2 when the heart is not beating and depending on do2
+    bm_vo2_ref: float = 100.0           # vo2 when the heart is not beating and depending on do2
+    bm_vo2_ref_factor: float = 1.0
+    bm_vo2_min: float = 80.0            # maximal reduction of baseline vo2
+    ecc_vo2_factor: float = 10.0
+    pva_vo2_factor: float = 10.0
+
+    bm_po2_set: float = 10.0       
+    bm_po2_max: float = 10.0
+    bm_po2_min: float = 0.0
+    bm_po2_tc: float = 5.0
+    bm_po2_g: float = 2.0               # maximal reduction of 20%   -10 * 2.0 = 20
+  
     heart_model: str = "Heart"
     aa_model: str = "AA"
     aa_cor_model: str = "AA_COR"
@@ -17,10 +29,15 @@ class Mob(BaseModel):
     rv_model: str = "RV"
 
     # dependent variables
+    bm_vo2: float = 0.0
+    ecc_vo2: float = 0.0
+    pva_vo2: float = 0.0
+    total_vo2: float = 0.0
     to2_co2: float = 0.0
     to2_in: float = 0.0
     ecc_lv: float = 0.0                         # pres_ms of the heart chamber 
     ecc_rv: float = 0.0
+    ecc: float = 0.0
     pva: float = 0.0                            # total pva of both ventricles
     stroke_work_lv: float = 0.0                 # stroke work of left ventricle
     stroke_work_rv: float = 0.0                 # stroke work of right ventricle
@@ -44,6 +61,9 @@ class Mob(BaseModel):
     _pv_area_rv: float = 0.0
     _sv_lv_cum: float = 0.0
     _sv_rv_cum: float = 0.0
+
+    _a_bm_vo2: float = 0.0
+    _d_bm_vo2: float = 0.0
 
     def init_model(self, model: object) -> bool:
         # initialize the base model
@@ -69,6 +89,7 @@ class Mob(BaseModel):
         # get the ecc from the heart chambers
         self.ecc_lv = self._lv.pres_ms
         self.ecc_rv = self._rv.pres_ms
+        self.ecc = self.ecc_lv + self.ecc_rv
 
         # calculate the pressure volume loop area
         self.pva = self.calc_pva()
@@ -78,10 +99,34 @@ class Mob(BaseModel):
             set_blood_composition(self._cor)
 
         # calculate the oxygen metabolism
-        self.oxygen_metabolism()
+        self.total_vo2 = self.oxygen_metabolism()
 
-    def oxygen_metabolism(self) -> None:
+    def oxygen_metabolism(self) -> float:
+        # get the po2 in mmHg from coronaries
         po2_cor = self._cor.aboxy["po2"]
+
+        # calculate the activation function of the baseline vo2
+        self._a_bm_vo2 = activation_function(po2_cor, self.bm_po2_max, self.bm_po2_set, self.bm_po2_min)
+
+        # calculate the gain depending on the reference and minimal baseline vo2 and po2 threshold from where the baseline vo2 is reduced
+        # this gain determines how much the baseline vo2 is reduced when the po2 drops below the threshold
+        self.bm_po2_g = ((self.bm_vo2_ref * self.bm_vo2_ref_factor) - self.bm_vo2_min) / (self.bm_po2_set - self.bm_po2_min)
+
+        # incorporate the time constant
+        self._d_bm_vo2 = self._t * ((1 / self.bm_po2_tc) * (-self._d_bm_vo2 + self._a_bm_vo2)) + self._d_bm_vo2
+
+        # calculate the baseline vo2
+        self.bm_vo2 = (self.bm_vo2_ref * self.bm_vo2_ref_factor) + self._d_bm_vo2 * self.bm_po2_g
+
+        # calculate the ecc vo2
+        self.ecc_vo2 = self.ecc * self.ecc_vo2_factor
+
+        # calculate the pva vo2
+        self.pva_vo2 = self.pva * self.pva_vo2_factor
+
+        # return the total voi2
+        return self.bm_vo2 + self.ecc_vo2 + self.pva_vo2
+
 
     def calc_pva(self) -> float:
         # detect the start of the cardiac cycle and calculate the area of the pv loop of the last cardiac cycle
