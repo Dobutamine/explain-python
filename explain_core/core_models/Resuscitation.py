dce4bc82155be11225c109bb3610926cb18b60ad
dobutamine <tajantonius@gmail.com>
2023-10-16 23:12:19 +0200

    update

--- explain_core/core_models/Resuscitation.py
+++ explain_core/core_models/Resuscitation.py
@@ -1,17 +1,19 @@
 import math
 from explain_core.base_models.BaseModel import BaseModel
 from explain_core.core_models.Container import Container
+from explain_core.core_models.Heart import Heart
 
 class Resuscitation(BaseModel):
     # independent parameters
     chest_comp_freq: float = 100.0          # number of chest compressions per minute
-    chest_comp_pres: float = 20.0           # pressure of the chest compressions in mmHg
+    chest_comp_pres: float = 60.0           # pressure of the chest compressions in mmHg
     vent_freq: float = 30.0                 # number of ventilations per minute
     vent_pres: float = 20.0                 # pressure of the ventilations in mmHg
     vent_fio2: float = 0.21                 # fio2 of the ventilations
     vent_insp_time: float = 1.0             # inspiration time of the ventilations
     vent_comp_ratio: float = 0.33           # ratio of ventilation to compressions
-    thorax_model: str = "THORAX"
+    thorax_model: str = "PC"
+    heart_model: str = "Heart"
 
     # dependent parameters
     cor_flow: float = 0.0                   # coronary flow in l/min
@@ -23,6 +25,7 @@ class Resuscitation(BaseModel):
 
     # local parameters
     _thorax: BaseModel = {}
+    _heart: Heart = {}
     _cpr_enabled: bool = False
     _comp_interval: float = 0.0
     _comp_duration: float = 0.0
@@ -33,6 +36,7 @@ class Resuscitation(BaseModel):
 
     _x: float = 0.0
     _x_step: float = 0.0
+    _prev_el_max_factor: float = 0.0
 
 
     def init_model(self, model: object) -> bool:
@@ -41,6 +45,7 @@ class Resuscitation(BaseModel):
 
         # get a reference to the heart
         self._thorax = self._model.models[self.thorax_model]
+        self._heart = self._model.models[self.heart_model]
 
         # signal that the ventilator model is initialized and return it
         self._is_initialized = True
@@ -48,6 +53,7 @@ class Resuscitation(BaseModel):
     
     def calc_model(self) -> None:
         if not self._cpr_enabled:
+            self._thorax.pres_cc = 0.0
             return
         
         # determine the interval between the compressions in seconds
@@ -70,7 +76,14 @@ class Resuscitation(BaseModel):
             # increase the compression timer
             self._comp_duration_counter += self._t
             # increase the compression pressure
-            self._comp_pres = math.sin(self._x)
+            self._comp_pres = math.sin(self._x) * self.chest_comp_pres
+           
+            self._heart._lv.pres_cc = self._comp_pres
+            self._heart._rv.pres_cc = self._comp_pres
+            self._heart._la.pres_cc = self._comp_pres
+            self._heart._ra.pres_cc = self._comp_pres
+            # self._model.models["CHEST_L"].pres_cc = self._comp_pres / 2.0
+            # self._model.models["CHEST_R"].pres_cc = self._comp_pres / 2.0
             self._x += self._x_step
 
 
@@ -89,13 +102,27 @@ class Resuscitation(BaseModel):
 
     
     def start_cpr(self) -> None:
+        self.cardiac_arrest(True)
         self._cpr_enabled = True
 
     def stop_cpr(self) -> None:
+        self.cardiac_arrest(False)
         self._cpr_enabled = False
 
     def cardiac_arrest(self, state) -> None:
-        pass
+        if state:
+            self._prev_el_max_factor = self._heart._lv.el_max_factor
+            self._model.models["Breathing"].breathing_enabled = False
+            self._heart._lv.el_max_factor = 0.0
+            self._heart._la.el_max_factor = 0.0
+            self._heart._rv.el_max_factor = 0.0
+            self._heart._ra.el_max_factor = 0.0
+        else:
+            self._model.models["Breathing"].breathing_enabled = True
+            self._heart._lv.el_max_factor = self._prev_el_max_factor
+            self._heart._la.el_max_factor = self._prev_el_max_factor
+            self._heart._rv.el_max_factor = self._prev_el_max_factor
+            self._heart._ra.el_max_factor = self._prev_el_max_factor
 
 
     
