97405defc9971423c27db34117d68aba904997b5
dobutamine <tajantonius@gmail.com>
2023-12-17 22:12:33 +0100

    update

--- explain_core/base_models/TimeVaryingElastance.py
+++ explain_core/base_models/TimeVaryingElastance.py
@@ -4,75 +4,71 @@ from explain_core.base_models.Capacitance import Capacitance
 
 class TimeVaryingElastance(Capacitance):
     # independent variables
-    u_vol: float = 0.0
-    u_vol_factor: float = 1.0
-    u_vol_ans_factor: float = 1.0
-    u_vol_scaling_factor: float = 1.0
-
-    el_base: float = 0.0
-    el_base_scaling_factor: float = 1.0
     el_min: float = 0.0
     el_min_factor: float = 1.0
-    el_min_scaling_factor: float = 1.0
-
     el_min_ans_factor: float = 1.0
     el_min_drug_factor: float = 1.0
+    el_min_mob_factor: float = 1.0
+    el_min_scaling_factor: float = 1.0
+
     el_max: float = 0.0
     el_max_factor: float = 1.0
+    el_max_ans_factor: float = 1.0
+    el_max_drug_factor: float = 1.0
+    el_max_mob_factor: float = 1.0
     el_max_scaling_factor: float = 1.0
 
-    el_max_ans_factor: float = 1.0
-    el_max_mob_factor: float = 1.0
-    el_max_drug_factor: float = 1.0
-    el_k: float = 0.0
-    el_k_factor: float = 1.0
-    el_k_scaling_factor: float = 1.0
-    act_factor: float = 0.0
-    ans_activity_factor: float = 1.0
-
     # dependent variables
-    vol: float = 0.0
-    pres: float = 0.0
-    pres_atm: float = 0.0
-    pres_mus: float = 0.0
-    pres_cc: float = 0.0
     pres_ed: float = 0.0
     pres_ms: float = 0.0
-    pres_ext: float = 0.0
-    pres_tm: float = 0.0
-    pres_in: float = 0.0
-    pres_out: float = 0.0
 
     # implement the calc_model method
     def calc_model(self) -> None:
-        # calculate the unstressed volume depending on the activity of the autonomic nervous system and scaling factor
-        _u_vol_base: float = self.u_vol * self.u_vol_factor * self.u_vol_scaling_factor
+        # calculate the minimal elastance depending on the scaling factor
+        _e_min_base: float = self.el_min * self.el_min_scaling_factor
+        # adjust the elastance depending on the activity of the external factor, autonomic nervous system and the drug model
+        _e_min: float = (
+            _e_min_base
+            + (self.el_min_factor * _e_min_base - _e_min_base)
+            + (self.el_min_ans_factor * _e_min_base - _e_min_base)
+            * self.ans_activity_factor
+            + (self.el_base_drug_factor * _e_min_base - _e_min_base)
+        )
+
+        # calculate the maximal elastance depending on the scaling factor
+        _e_max_base: float = self.el_max * self.el_max_scaling_factor
+        # adjust the elastance depending on the activity of the external factor, autonomic nervous system, the drug model and mob model
+        _e_max: float = (
+            _e_max_base
+            + (self.el_max_factor * _e_max_base - _e_max_base)
+            + (self.el_max_ans_factor * _e_max_base - _e_max_base)
+            * self.ans_activity_factor
+            + (self.el_max_drug_factor * _e_max_base - _e_max_base)
+            + (self.el_max_mob_factor * _e_max_base - _e_max_base)
+        )
+
+        # calculate the non-linear elastance factor depending on the scaling factor
+        _el_k_base: float = self.el_k * self.el_k_scaling_factor
+
+        # adjust the non-linear elastance depending on the activity of the external factor, autonomic nervous system and the drug model
+        _el_k: float = (
+            _el_k_base
+            + (self.el_k_factor * _el_k_base - _el_k_base)
+            + (self.el_k_ans_factor * _el_k_base - _el_k_base)
+            * self.ans_activity_factor
+            + (self.el_k_drug_factor * _el_k_base - _el_k_base)
+        )
+
+        # calculate the unstressed volume depending on the scaling factor
+        _u_vol_base: float = self.u_vol * self.u_vol_scaling_factor
+
+        # adjust the unstressed volume depending on the activity of the external factor, autonomic nervous system and the drug model
         _u_vol: float = (
             _u_vol_base
+            + (_u_vol_base * self.u_vol_factor - _u_vol_base)
             + (_u_vol_base * self.u_vol_ans_factor - _u_vol_base)
             * self.ans_activity_factor
-        )
-
-        _emin_base: float = (
-            self.el_min * self.el_min_factor * self.el_min_scaling_factor
-        )
-        _emax_base: float = (
-            self.el_max * self.el_max_factor * self.el_max_scaling_factor
-        )
-
-        emin: float = (
-            _emin_base
-            + (self.el_min_ans_factor * _emin_base - _emin_base)
-            * self.ans_activity_factor
-            + (self.el_base_drug_factor * _emin_base - _emin_base)
-        )
-
-        emax: float = (
-            _emax_base
-            + (self.el_max_ans_factor * _emax_base - _emax_base)
-            * self.ans_activity_factor
-            + (self.el_max_drug_factor * _emax_base - _emax_base)
-            + (self.el_max_mob_factor * _emax_base - _emax_base)
+            + (_u_vol_base * self.u_vol_drug_factor - _u_vol_base)
         )
 
         # calculate the volume difference
@@ -80,13 +76,13 @@ class TimeVaryingElastance(Capacitance):
 
         # calculate the pressure
         self.pres_ed = (
-            emin * vol_diff
+            _e_min * vol_diff
             + self.el_k
             * self.el_k_factor
             * self.el_k_scaling_factor
             * math.pow(vol_diff, 2)
         )
-        self.pres_ms = emax * vol_diff
+        self.pres_ms = _e_max * vol_diff
         if self.pres_ms < self.pres_ed:
             self.pres_ms = self.pres_ed
 
