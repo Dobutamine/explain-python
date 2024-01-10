5af1a48d906df19fdeb0528073596e1112830a0f
dobutamine <tajantonius@gmail.com>
2024-01-10 10:15:59 +0100

    update

---
+++ explain_core/core_models/Blood.py
@@ -0,0 +1,155 @@
+from explain_core.base_models.BaseModel import BaseModel
+from explain_core.functions.BloodComposition import set_blood_composition
+
+
+class Blood(BaseModel):
+    # independent variables
+    solutes: dict[str, float] = {}
+    aboxy: dict[str, float] = {}
+
+    def init_model(self, model: object) -> bool:
+        super().init_model(model)
+
+        # find all models containing blood and set it's solutes, acidbase and oxygenation variables
+        for model in self._model.models.values():
+            if (
+                model.model_type == "BloodCapacitance"
+                or model.model_type == "BloodTimeVaryingElastance"
+            ):
+                # check whether the solutes are already set
+                if not model.solutes:
+                    # fill the solutes
+                    model.solutes = {**self.solutes}
+                if not model.aboxy:
+                    model.aboxy = {**self.aboxy}
+                    # calculate the to2 from the spo2 and hemoglobin
+                    model.aboxy["to2"] = (
+                        (
+                            1.36
+                            * (model.aboxy["hemoglobin"] / 0.6206)
+                            * model.aboxy["so2"]
+                            / 100.0
+                        )
+                        * 10.0
+                    ) / 25.5
+
+        return self._is_initialized
+
+    # implement the calc_model method as this derives from the BaseModel class
+    def calc_model(self) -> None:
+        pass
+
+    # reset the total blood volume
+    def set_total_blood_volume(self, new_blood_volume: float) -> None:
+        current_blood_volume = self.get_total_blood_volume(output=False)
+        new_blood_volume = new_blood_volume
+        # divide the new blood volume over all blood holding capacitances
+        # while abs(current_blood_volume - new_blood_volume) > 0.001:
+        for model in self._model.models.values():
+            if "BloodCapacitance" in model.model_type:
+                if model.is_enabled:
+                    try:
+                        # calculate the current fraction of the blood volume in this blood containing capacitance
+                        current_fraction = model.vol / current_blood_volume
+
+                        v = model.vol
+                        vu = model.u_vol
+                        # calculate the new fraction
+                        f = 0.0
+                        if model.vol > 0.0:
+                            f = model.u_vol / model.vol
+
+                        # add the same fraction of the desired volume change to the blood containing capacitance
+                        model.vol += current_fraction * (
+                            new_blood_volume - current_blood_volume
+                        )
+
+                        # change the unstressed volume
+                        model.u_vol = model.vol * f
+
+                        # guard for negative volumes
+                        if model.vol < 0.0:
+                            model.vol = 0.0
+                    except:
+                        current_fraction = 0
+
+            if "BloodTimeVaryingElastance" in model.model_type:
+                if model.is_enabled:
+                    try:
+                        # calculate the current fraction of the blood volume in this blood containing capacitance
+                        current_fraction = model.vol / current_blood_volume
+
+                        v = model.vol
+                        vu = model.u_vol
+                        # calculate the new fraction
+                        f = 0.0
+                        if model.vol > 0.0:
+                            f = model.u_vol / model.vol
+
+                        # add the same fraction of the desired volume change to the blood containing capacitance
+                        model.vol += current_fraction * (
+                            new_blood_volume - current_blood_volume
+                        )
+
+                        # change the unstressed volume
+                        model.u_vol = model.vol * f
+
+                        # guard for negative volumes
+                        if model.vol < 0.0:
+                            model.vol = 0.0
+                    except:
+                        current_fraction = 0
+
+    # return the total blood volume
+    def get_total_blood_volume(self, output=True) -> float:
+        total_blood_volume: float = 0.0
+
+        for model in self._model.models.values():
+            if "Blood" in model.model_type:
+                if model.is_enabled:
+                    try:
+                        total_blood_volume += model.vol
+                    except:
+                        total_blood_volume += 0.0
+
+            # if "Blood" in model.model_type and model.is_enabled:
+            #     total_blood_volume += model.vol
+
+        if output:
+            print(
+                f"Total blood volume = {total_blood_volume * 1000.0} ml ({total_blood_volume * 1000.0 / self._model.weight} ml/kg)"
+            )
+
+        return total_blood_volume
+
+    # set the blood composition of a blood containing model
+    def set_blood_composition(self, bc: dict) -> bool:
+        if "Blood" in bc.model_type:
+            set_blood_composition(bc)
+            return True
+
+        return None
+
+    # set the concentration of a solute in the blood
+    def set_solute_concentration(self, solute_name: str, solute_conc: float) -> None:
+        self.solutes[solute_name]: float = solute_conc
+
+        for model in self._model.models.values():
+            if (
+                model.model_type == "BloodCapacitance"
+                or model.model_type == "BloodTimeVaryingElastance"
+            ):
+                # fill the solutes
+                model.solutes[solute_name] = solute_conc
+
+    # set the concentration of a aboxy solute in the blood
+    def set_aboxy_concentration(self, solute_name: str, solute_conc: float) -> None:
+        self.aboxy[solute_name]: float = solute_conc
+
+        for model in self._model.models.values():
+            if (
+                model.model_type == "BloodCapacitance"
+                or model.model_type == "BloodTimeVaryingElastance"
+            ):
+                # fill the solutes
+                model.aboxy[solute_name] = solute_conc
---
+++ explain_core/core_models/Lungs.py
@@ -0,0 +1,90 @@
+from explain_core.base_models.BaseModel import BaseModel
+from explain_core.core_models.GasCapacitance import GasCapacitance
+from explain_core.core_models.GasExchanger import GasExchanger
+from explain_core.base_models.Container import Container
+from explain_core.base_models.Resistor import Resistor
+
+
+class Lungs(BaseModel):
+    # independent parameters
+    upper_airways: str = ["MOUTH_DS"]
+    dead_space: str = ["DS"]
+    thorax: str = "THORAX"
+    chestwall: str = ["CHEST_L", "CHEST_R"]
+    alveolar_spaces: str = ["ALL", "ALR"]
+    lower_airways: str = ["DS_ALL", "DS_ALR"]
+    gas_exchangers: str = ["GASEX_LL", "GASEX_RL"]
+
+    # local parameters
+    _upper_airways: Resistor = []
+    _dead_space: GasCapacitance = []
+    _lower_airways: Resistor = []
+    _alveolar_spaces: GasCapacitance = []
+    _thorax: Container = []
+    _chestwall: Container = []
+    _gas_exchangers: Resistor = []
+
+    def init_model(self, model: object) -> bool:
+        # initialize the base model
+        super().init_model(model)
+
+        # get all the model components
+        for uaw in self.upper_airways:
+            self._upper_airways.append(self._model.models[uaw])
+
+        for ds in self.dead_space:
+            self._dead_space.append(self._model.models[ds])
+
+        for law in self.lower_airways:
+            self._lower_airways.append(self._model.models[law])
+
+        for alvs in self.alveolar_spaces:
+            self._alveolar_spaces.append(self._model.models[alvs])
+
+        for th in self.thorax:
+            self._thorax.append(self._model.models[th])
+
+        for cw in self.chestwall:
+            self._chestwall.append(self._model.models[cw])
+
+        for gasex in self.gas_exchangers:
+            self._gas_exchangers.append(self._model.models[gasex])
+
+        # signal that the ventilator model is initialized and return it
+        self._is_initialized = True
+        return self._is_initialized
+
+    def calc_model(self) -> None:
+        pass
+
+    def change_dif_o2(self, dif_o2_change):
+        if dif_o2_change > 0.0:
+            for gasex in self._gas_exchangers:
+                gasex.dif_o2_factor = dif_o2_change
+
+    def change_dif_co2(self, dif_co2_change):
+        if dif_co2_change > 0.0:
+            for gasex in self._gas_exchangers:
+                gasex.dif_co2_factor = dif_co2_change
+
+    def change_lung_compliance(self, comp_change):
+        if comp_change > 0.0:
+            for alv_space in self._alveolar_spaces:
+                alv_space.el_base_factor = 1.0 / comp_change
+
+    def change_chestwall_compliance(self, comp_change):
+        if comp_change > 0.0:
+            for cw in self._chestwall:
+                cw.el_base_factor = 1.0 / comp_change
+
+    def change_upper_airway_resistance(self, res_change):
+        if res_change > 0.0:
+            for uaw in self._upper_airways:
+                uaw.r_for_factor = res_change
+                uaw.r_back_factor = res_change
+
+    def change_lower_airway_resistance(self, res_change):
+        if res_change > 0.0:
+            for law in self._lower_airways:
+                law.r_for_factor = res_change
+                law.r_back_factor = res_change
