729ec30ad5c155c95f98bfb889e85c0db5ee1521
dobutamine <tajantonius@gmail.com>
2024-07-07 22:06:08 +0200

    update

--- explain_core/core_models/Gas.py
+++ explain_core/core_models/Gas.py
@@ -1,4 +1,5 @@
 import math
+from explain_core.helpers.GasComposition import set_gas_composition
 
 
 class Gas:
@@ -12,18 +13,58 @@ class Gas:
         self.description: str = ""
         self.is_enabled: bool = False
         self.dependencies: list = []
+        self.gas_containing_components: list = []
+        self.humidity_settings = None
+        self.temp_settings = None
+        self.pres_atm = 760.0
+        self.fio2 = 0.21
+        self.humidity = 0.5
+        self.temp = 20.0
 
         # dependent properties
+        self.total_gas_volume = 0.0
+        self.po2_alv = self.pco2_alv = 0.0
 
         # local properties
         self._model_engine: object = model_ref
         self._is_initialized: bool = False
         self._t: float = model_ref.modeling_stepsize
+        self._gas_constant = 62.36367
+        self._update_interval = 2.0
+        self._update_counter = 0.0
 
     def init_model(self, **args: dict[str, any]):
         # set the values of the independent properties
         for key, value in args.items():
             setattr(self, key, value)
+
+        # set the atmospheric pressure in all gas capacitances
+        for model_name in self.gas_containing_components:
+            self._model_engine.models[model_name].pres_atm = self.pres_atm
+
+        # set the temperatures
+        for model_name, temp in self.temp_settings.items():
+            if self._model_engine.models[model_name].temp == 0.0:
+                self._model_engine.models[model_name].temp = temp
+                self._model_engine.models[model_name].target_temp = temp
+
+        # set the humidity
+        for model_name, humidity in self.humidity_settings.items():
+            if self._model_engine.models[model_name].humidity == 0.0:
+                self._model_engine.models[model_name].humidity = humidity
+
+        # calculate the gas composition if not done set already
+        for model_name in self.gas_containing_components:
+            if self._model_engine.models[model_name].co2 == 0.0:
+                set_gas_composition(
+                    self._model_engine.models[model_name],
+                    self.fio2,
+                    self._model_engine.models[model_name].temp,
+                    self._model_engine.models[model_name].humidity,
+                )
+
+        # get the current total gas volume
+        self.total_gas_volume = self.get_total_gas_volume()
 
         # flag that the model is initialized
         self._is_initialized = True
@@ -35,4 +76,82 @@ class Gas:
 
     # actual model calculations are done here
     def calc_model(self):
-        pass
+        if self._update_counter > self._update_interval:
+            self._update_counter = 0.0
+
+            self.temp = self._model_engine.models["OUT"].temp
+            self.humidity = self._model_engine.models["OUT"].humidity
+
+            self.get_total_gas_volume()
+
+        self._update_counter += self._t
+
+    # calculate the total gas volume
+    def get_total_gas_volume(self):
+        total_volume = 0.0
+        for model_name in self.gas_containing_components:
+            if self._model_engine.models[model_name].is_enabled:
+                total_volume += self._model_engine.models[model_name].vol
+
+        return total_volume
+
+    def set_total_gas_volume(self, new_gas_volume):
+        current_volume = self.get_total_gas_volume()
+        gas_volume_change = new_gas_volume / current_volume
+
+        for model in self.gas_containing_components:
+            m = self._model_engine.models[model]
+            if m.is_enabled and not m.fixed_composition:
+                m.vol = m.vol * gas_volume_change
+                m.u_vol = m.u_vol * gas_volume_change
+
+    def set_new_atmospheric_pressure(self, new_p_atm):
+        if new_p_atm > 0.0:
+            self.pres_atm = new_p_atm
+            for model in self.gas_containing_components:
+                self._model_engine.models[model].pres_atm = self.pres_atm
+
+    def set_new_temperature(self, new_temp, sites=["OUT", "MOUTH"]):
+        if type(sites) == str:
+            sites = [sites]
+
+        if new_temp >= 0.0 and new_temp <= 100.0:
+            for site in sites:
+                self.temp_settings[site] = new_temp
+                self._model_engine.models[site].temp = self.temp
+                self._model_engine.models[site].target_temp = self.temp
+                set_gas_composition(
+                    self._model_engine.models[site],
+                    self.fio2,
+                    new_temp,
+                    self._model_engine.models[site].humidity,
+                )
+
+    def set_new_fio2(self, new_fio2, sites=["OUT", "MOUTH"]):
+        if type(sites) == str:
+            sites = [sites]
+
+        if new_fio2 >= 0.21 and new_fio2 <= 1.0:
+            for site in sites:
+                self.fio2 = new_fio2
+                set_gas_composition(
+                    self._model_engine.models[site],
+                    self.fio2,
+                    self._model_engine.models[site].temp,
+                    self._model_engine.models[site].humidity,
+                )
+
+    def set_new_humidity(self, new_humidity, sites=["OUT", "MOUTH"]):
+        if type(sites) == str:
+            sites = [sites]
+
+        if new_humidity >= 0.0 and new_humidity <= 1.0:
+            for site in sites:
+                self.humidity_settings[site] = new_humidity
+                self._model_engine.models[site].humidity = self.new_humidity
+                set_gas_composition(
+                    self._model_engine.models[site],
+                    self.fio2,
+                    self._model_engine.models[site].temp,
+                    new_humidity,
+                )
--- explain_core/core_models/GasExchanger.py
+++ explain_core/core_models/GasExchanger.py
@@ -1,5 +1,7 @@
 import math
 
+from explain_core.helpers.BloodComposition import set_blood_composition
+
 
 class GasExchanger:
     # static properties
@@ -12,18 +14,41 @@ class GasExchanger:
         self.description: str = ""
         self.is_enabled: bool = False
         self.dependencies: list = []
+        self.dif_o2 = 0.01
+        self.dif_o2_factor = 1.0
+        self.dif_o2_scaling_factor = 1.0
+        self.dif_co2 = 0.01
+        self.dif_co2_factor = 1.0
+        self.dif_co2_scaling_factor = 1.0
+        self.comp_blood = ""
+        self.comp_gas = ""
 
         # dependent properties
+        self.flux_o2 = 0
+        self.flux_co2 = 0
 
         # local properties
         self._model_engine: object = model_ref
         self._is_initialized: bool = False
         self._t: float = model_ref.modeling_stepsize
+        self._blood = None
+        self._gas = None
 
     def init_model(self, **args: dict[str, any]):
         # set the values of the independent properties
         for key, value in args.items():
             setattr(self, key, value)
+
+        # get a reference to the blood and gas capacitances
+        if type(self.comp_blood) == str:
+            self._blood = self._model_engine.models[self.comp_blood]
+        else:
+            self._blood = self.comp_blood
+
+        if type(self.comp_gas) == str:
+            self._gas = self._model_engine.models[self.comp_gas]
+        else:
+            self._gas = self.comp_gas
 
         # flag that the model is initialized
         self._is_initialized = True
@@ -35,4 +60,60 @@ class GasExchanger:
 
     # actual model calculations are done here
     def calc_model(self):
-        pass
+        # set the blood composition
+        set_blood_composition(self._blood)
+
+        # get the partial pressures and gas concentrations from the components
+        po2_blood = self._blood.aboxy["po2"]
+        pco2_blood = self._blood.aboxy["pco2"]
+        to2_blood = self._blood.aboxy["to2"]
+        tco2_blood = self._blood.aboxy["tco2"]
+
+        co2_gas = self._gas.co2
+        cco2_gas = self._gas.cco2
+        po2_gas = self._gas.po2
+        pco2_gas = self._gas.pco2
+
+        # calculate the O2 flux from the blood to the gas compartment
+        self.flux_o2 = (
+            (po2_blood - po2_gas)
+            * self.dif_o2
+            * self.dif_o2_factor
+            * self.dif_o2_scaling_factor
+            * self._t
+        )
+
+        # calculate the new O2 concentrations of the gas and blood compartments
+        new_to2_blood = (to2_blood * self._blood.vol - self.flux_o2) / self._blood.vol
+        if new_to2_blood < 0:
+            new_to2_blood = 0.0
+
+        new_co2_gas = (co2_gas * self._gas.vol + self.flux_o2) / self._gas.vol
+        if new_co2_gas < 0:
+            new_co2_gas = 0.0
+
+        # calculate the CO2 flux from the blood to the gas compartment
+        self.flux_co2 = (
+            (pco2_blood - pco2_gas)
+            * self.dif_co2
+            * self.dif_co2_factor
+            * self.dif_co2_scaling_factor
+            * self._t
+        )
+
+        # calculate the new CO2 concentrations of the gas and blood compartments
+        new_tco2_blood = (
+            tco2_blood * self._blood.vol - self.flux_co2
+        ) / self._blood.vol
+        if new_tco2_blood < 0:
+            new_tco2_blood = 0.0
+
+        new_cco2_gas = (cco2_gas * self._gas.vol + self.flux_co2) / self._gas.vol
+        if new_cco2_gas < 0:
+            new_cco2_gas = 0.0
+
+        # transfer the new concentrations
+        self._blood.aboxy["to2"] = new_to2_blood
+        self._blood.aboxy["tco2"] = new_tco2_blood
+        self._gas.co2 = new_co2_gas
+        self._gas.cco2 = new_cco2_gas
