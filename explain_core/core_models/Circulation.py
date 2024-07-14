85d574b860eeec3e84360784a5e865faddf57d84
dobutamine <tajantonius@gmail.com>
2024-07-14 11:31:23 +0200

    update

--- explain_core/core_models/Circulation.py
+++ explain_core/core_models/Circulation.py
@@ -25,6 +25,7 @@ class Circulation:
         self.venpool_change = 1.0
         self.pulmartcomp_change = 1.0
         self.systartcomp_change = 1.0
+        self.coarc_change = 1.0
         self.vencomp_change = 1.0
         self.svr_ans_factor = 1.0
         self.pvr_ans_factor = 1.0
@@ -37,7 +38,6 @@ class Circulation:
         self._model_engine: object = model_ref
         self._is_initialized: bool = False
         self._t: float = model_ref.modeling_stepsize
-        self._ans_models = []
         self._svr_targets = []
         self._pvr_targets = []
         self._venpool_targets = []
@@ -85,3 +85,72 @@ class Circulation:
 
             for vpt in self._venpool_targets:
                 vpt.u_vol_ans_factor = self.venpool_ans_factor
+
+    def change_pvr(self, change_forward, change_backward=-1.0):
+        if change_forward > 0.0:
+            self.pvr_change = change_forward
+            for pvrt in self._pvr_targets:
+                pvrt.r_for_factor = change_forward
+                pvrt.r_back_factor = change_forward
+                if change_backward > 0.0:
+                    pvrt.r_back_factor = change_backward
+
+    def change_svr(self, change_forward, change_backward=-1.0):
+        if change_forward > 0.0:
+            self.svr_change = change_forward
+            for svrt in self._svr_targets:
+                svrt.r_for_factor = change_forward
+                svrt.r_back_factor = change_forward
+                if change_backward > 0.0:
+                    svrt.r_back_factor = change_backward
+
+    def change_venpool(self, change):
+        if change > 0.0:
+            self.venpool_change = change
+            for vpt in self._venpool_targets:
+                vpt.u_vol_factor = change
+
+    def change_coarc(self, change_forward):
+        if change_forward > 0.0:
+            self.coarc_change = change_forward
+            self._model_engine.models["AA_AAR"].r_for_factor = change_forward
+            self._model_engine.models["AA_AAR"].r_back_factor = change_forward
+
+    def change_syst_arterial_elastance(self, change):
+        if change > 0.0:
+            self.systartcomp_change = change
+            for target in self.systemic_arteries:
+                self._model_engine.models[target].el_base_factor = change
+
+    def change_pulm_arterial_elastance(self, change):
+        if change > 0.0:
+            self.pulmartcomp_change = change
+            for target in self.pulmonary_arteries:
+                self._model_engine.models[target].el_base_factor = change
+
+    def change_venous_elastance(self, change):
+        if change > 0.0:
+            self.vencomp_change = change
+            for target in self.systemic_veins:
+                self._model_engine.models[target].el_base_factor = change
+
+    def get_total_blood_volume(self):
+        total_volume = 0.0
+        for target in self.blood_containing_components:
+            if self._model_engine.models[target].is_enabled:
+                total_volume += self._model_engine.models[target].vol
+        self.total_blood_volume = total_volume
+        return total_volume
+
+    def set_total_blood_volume(self, new_blood_volume):
+        current_blood_volume = self.get_total_blood_volume()
+        blood_volume_change = new_blood_volume / current_blood_volume
+        for target in self.blood_containing_components:
+            if self._model_engine.models[target].is_enabled:
+                self._model_engine.models[target].vol = (
+                    self._model_engine.models[target].u_vol * blood_volume_change
+                )
+                self._model_engine.models[target].u_vol = (
+                    self._model_engine.models[target].u_vol * blood_volume_change
+                )
+        self.total_blood_volume = self.get_total_blood_volume()
--- explain_core/core_models/Lungs.py
+++ explain_core/core_models/Lungs.py
@@ -12,18 +12,69 @@ class Lungs:
         self.description: str = ""
         self.is_enabled: bool = False
         self.dependencies: list = []
+        self.upper_airways = []
+        self.dead_space = []
+        self.thorax = []
+        self.chestwall = []
+        self.alveolar_spaces = []
+        self.lower_airways = []
+        self.gas_exchangers = []
+        self.lung_shunts = []
 
         # dependent properties
+        self.dif_o2_change = 1.0
+        self.dif_co2_change = 1.0
+        self.dead_space_change = 1.0
+        self.lung_comp_change = 1.0
+        self.chestwall_comp_change = 1.0
+        self.thorax_comp_change = 1.0
+        self.upper_aw_res_change = 1.0
+        self.lower_aw_res_change = 1.0
+        self.lung_shunt_change = 1.0
+        self.atelectasis_change = 1.0
 
         # local properties
         self._model_engine: object = model_ref
         self._is_initialized: bool = False
         self._t: float = model_ref.modeling_stepsize
+        self._upper_airways = []
+        self._dead_space = []
+        self._thorax = []
+        self._chestwall = []
+        self._alveolar_spaces = []
+        self._lower_airways = []
+        self._gas_exchangers = []
+        self._lung_shunts = []
 
     def init_model(self, **args: dict[str, any]):
         # set the values of the independent properties
         for key, value in args.items():
             setattr(self, key, value)
+
+        # store a reference to the necessary models
+        for target in self.upper_airways:
+            self._upper_airways.append(self._model_engine.models[target])
+
+        for target in self.dead_space:
+            self._dead_space.append(self._model_engine.models[target])
+
+        for target in self.lower_airways:
+            self._lower_airways.append(self._model_engine.models[target])
+
+        for target in self.chestwall:
+            self._chestwall.append(self._model_engine.models[target])
+
+        for target in self.alveolar_spaces:
+            self._alveolar_spaces.append(self._model_engine.models[target])
+
+        for target in self.gas_exchangers:
+            self._gas_exchangers.append(self._model_engine.models[target])
+
+        for target in self.lung_shunts:
+            self._lung_shunts.append(self._model_engine.models[target])
+
+        for target in self.thorax:
+            self._thorax.append(self._model_engine.models[target])
 
         # flag that the model is initialized
         self._is_initialized = True
@@ -36,3 +87,74 @@ class Lungs:
     # actual model calculations are done here
     def calc_model(self):
         pass
+
+    def change_lung_shunt(self, change_forward, change_backward=-1):
+        if change_forward > 0.0:
+            self.lung_shunt_change = change_forward
+            for target in self._lung_shunts:
+                target.r_for_factor = change_forward
+                target.r_back_factor = change_forward
+                if change_backward >= 0.0:
+                    target.r_back_factor = change_backward
+
+    def change_atelectasis(self, change):
+        if change > 0.0:
+            self.atelectasis_change = change
+
+    def change_dead_space(self, change):
+        if change > 0.0:
+            self.dead_space_change = change
+            for target in self._dead_space:
+                target.u_vol_factor = change
+
+    def change_dif(self, change):
+        if change > 0.0:
+            self.dif_o2_change = change
+            self.dif_co2_change = change
+            for target in self._gas_exchangers:
+                target.dif_o2_factor = change
+                target.dif_co2_factor = change
+
+    def change_dif_o2(self, change):
+        if change > 0.0:
+            self.dif_o2_change = change
+            for target in self._gas_exchangers:
+                target.dif_o2_factor = change
+
+    def change_dif_co2(self, change):
+        if change > 0.0:
+            self.dif_co2_change = change
+            for target in self._gas_exchangers:
+                target.dif_co2_factor = change
+
+    def change_lower_airway_resistance(self, change_forward, change_backward=-1):
+        if change_forward > 0.0:
+            self.lower_aw_res_change = change_forward
+            for target in self._lower_airways:
+                target.r_for_factor = change_forward
+                target.r_back_factor = change_forward
+
+    def change_upper_airway_resistance(self, change_forward, change_backward=-1):
+        if change_forward > 0.0:
+            self.lower_aw_res_change = change_forward
+            for target in self._upper_airways:
+                target.r_for_factor = change_forward
+                target.r_back_factor = change_forward
+
+    def change_thorax_compliance(self, change):
+        if change > 0.0:
+            self.thorax_comp_change = change
+            for target in self._thorax:
+                target.el_base_factor = 1.0 / change
+
+    def change_lung_compliance(self, change):
+        if change > 0.0:
+            self.lung_comp_change = change
+            for target in self._alveolar_spaces:
+                target.el_base_factor = 1.0 / change
+
+    def change_chestwall_compliance(self, change):
+        if change > 0.0:
+            self.chestwall_comp_change = change
+            for target in self._chestwall:
+                target.el_base_factor = 1.0 / change
