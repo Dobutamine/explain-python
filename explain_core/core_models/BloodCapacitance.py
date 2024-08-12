3f78f68d825d04c429767faa3f1e44f141f099e3
dobutamine <tajantonius@gmail.com>
2024-08-12 23:41:26 +0200

    update

--- explain_core/core_models/BloodCapacitance.py
+++ explain_core/core_models/BloodCapacitance.py
@@ -3,6 +3,7 @@ class BloodCapacitance:
     model_type: str = "BloodCapacitance"
     model_interface: list = []
 
+    # works
     def __init__(self, model_ref: object, name: str = ""):
         # initialize independent properties
         self.name: str = name
